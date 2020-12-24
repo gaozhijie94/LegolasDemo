94c56c2673215c0ab529838c0084ae7c0ea62262
gaozhijie <gzj941001@icloud.com>
2020-12-24 16:36:42 +0800

    加了注释

---
+++ Shared/LegolasDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegolasDemoApp.swift
+//  Shared
+//
+//  Created by Zhijie Gao on 2020/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LegolasDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
